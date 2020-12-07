6e5e62fa7bba5c4b74ec6a45c998f59565b254a1
Konrad Merz <konrad@graylog.com>
2020-12-07 13:27:52 +0100

    Fix layout for dashboards which needs an additional row

--- graylog2-web-interface/src/views/components/Search.tsx
+++ graylog2-web-interface/src/views/components/Search.tsx
@@ -82,11 +82,11 @@ const SearchArea: StyledComponent<{}, void, any> = styled(AppContentGrid)`
   }
 `;
 
-const SearchLayoutContainer = styled.div`
+const SearchLayoutContainer = styled.div(({ isDashboard }) => css`
   height: 100%;
   display: grid;
-  grid-template-rows: min-content auto min-content;
-`;
+  ${isDashboard ? 'grid-template-rows: min-content auto min-content;' : 'grid-template-rows: min-content min-content auto min-content;'}
+`);
 
 const ConnectedSidebar = connect(
   Sidebar,
